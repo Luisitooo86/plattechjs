e155bbb34527bd9c179e827410c0982c14dc90fb
Luis Adrian C. Balita <22-61407@g.batstate-u.edu.ph>
2025-11-23 23:35:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve all files in /public
+// Serve public folder
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
